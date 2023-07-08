b6e34d1dde65f9bdb5ec747809a89698382d717f
Meddeb Montassar <montassarmeddeb729@gmail.com>
2023-07-08 06:30:12 +0100

    first_commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RgisterComponent } from './components/rgister/rgister.component';
+import { LoginComponent } from './components/login/login.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { UploadComponent } from './components/upload/upload.component';
+import { CoursesComponent } from './components/courses/courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'',redirectTo:'login', pathMatch:'full'},
+ {path:'login',component:LoginComponent},
+{path:'home',component:HomeComponent},
+{path:'register',component:RgisterComponent},
+{path:'sidenav',component:SideNavComponent},
+{path:'upload',component:UploadComponent},
+{path:'courses',component:CoursesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RgisterComponent } from './components/rgister/rgister.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { ExploreComponent } from './components/explore/explore.component';
+import { UploadComponent } from './components/upload/upload.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CoursesComponent } from './components/courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RgisterComponent,
+    SideNavComponent,
+    ExploreComponent,
+    UploadComponent,
+    NavbarComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  exports:[
+  SideNavComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
